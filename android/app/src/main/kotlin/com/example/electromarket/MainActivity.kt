c4b969ae3cf2aedb61dfeac6606f3213b0e31ba6
Brightnyandiko <carlsonbright98@gmail.com>
2025-01-28 17:15:23 +0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/electromarket/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.electromarket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      body: Center(
+        child: Column(
+          children: [
+            //logo
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Image.asset(
+                'lib/images/nike.png',
+                height: 240,
+              ),
+            ),
+            //title
+            const Text(
+                "Just Do It",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25
+                ),
+            ),
+
+            const SizedBox(height: 24),
+
+            //sub title
+            const Text(
+              "Brand new sneakers and custom kicks made with premium quality",
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 48),
+
+
+            //start now button
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                padding: const EdgeInsets.all(24),
+                child: const Center(
+                  child: Text(
+                      "Shop Now",
+                      style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
